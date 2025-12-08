4b874065531999f9197b449acd990e4c35a3e68e
GersonESS <gebhsantos@gmail.com>
2025-12-08 11:14:12 -0300

    a

---
+++ 01/todo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div>
+        <h1>Todo App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
